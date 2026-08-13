43e6f92d453189bdfc95004ab8e4c891c05383a1
dcr-cmyk648 <275179677+dcr-cmyk648@users.noreply.github.com>
2026-08-13 18:48:37 -0400

    Finalize v0.10.8 release artifacts

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786659567|5825427';
+const CACHE_VERSION = '1786661106|7182708';
 /** @type {string} */
 const CACHE_PREFIX = 'One Foot Per Sec-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
